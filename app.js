d5030be4c77f9f773907c5ff368b2be50bda59ec
Erik Gillespie <erik.gillespie@gmail.com>
2016-03-03 14:17:17 -0500

    Restructured routes as api

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var partials = require('./routes/partials');
+var routes = require('./api/index');
+var partials = require('./api/partials');
+var games = require('./api/games/games');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/partials', partials);
+app.use('/games', games);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
